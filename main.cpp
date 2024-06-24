1ab931c75b948cfbcf74685228e97637dd351f29
SweIsHere <angel.tribeno@utec.edu.pe>
2024-06-24 14:30:02 -0500

    Cambios en archivo Raw. (2)

--- main.cpp
+++ main.cpp
@@ -119,7 +119,7 @@ int main() {
     auto peliculas = leerCSV(nombreArchivo);
 
     // Buscar la información de "A Hard Day's Night" - Ejemplo correcto: "Henry: Portrait of a Serial Killer"
-    string tituloBusqueda = "A Hard Day's Night";
+    string tituloBusqueda = "The Shop Around the Corner";
 
     // Usar la sintaxis y funciones de unordered_map para buscar y mostrar la película
     auto it = peliculas.find(tituloBusqueda);
